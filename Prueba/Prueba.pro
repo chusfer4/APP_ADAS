3e4a4cc335f1e665ddf09197fe231385f86c3334
chusfer4 <chusfer4@hotmail.com>
2017-04-13 19:25:56 +0200

    Prueba c++

--- Prueba/Prueba.pro
+++ Prueba/Prueba.pro
@@ -3,4 +3,8 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+INCLUDEPATH += C:/Users/chusf/Documents/opencv/build/include
+
+
+
 SOURCES += main.cpp
--- Prueba/main.cpp
+++ Prueba/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
-using namespace std;
+//using namespace cv;
 
-int main(int argc, char *argv[])
+int main(int, char**)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+cv::VideoCapture cap(0); // open the default camera
+//Video Capture cap(path_to_video); // open the video file
+if(!cap.isOpened())  // check if we succeeded
+    return -1;
+
+cv::namedWindow("Video",1);
+for(;;)
+{
+    cv::Mat frame;
+    cap >> frame; // get a new frame from camera
+    imshow("Video", frame);
+    if(cv::waitKey(30) >= 0) break;
+}
+// the camera will be deinitialized automatically in VideoCapture destructor
+return 0;
 }
